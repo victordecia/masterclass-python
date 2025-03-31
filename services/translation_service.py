36a0d2517dd2ec5d263437e438da2f9bd06cb2e2
Victor Costa <victor.costa@agroboard.tec.br>
2025-03-31 19:09:27 -0300

    refactor service using CLEAN principles

--- services/translation_service.py
+++ services/translation_service.py
@@ -1,34 +1,66 @@
 import string
 
+# Constants
+VOWELS = "aeiouAEIOU"
+PUNCTUATION = string.punctuation
+
+def has_punctuation(word: str) -> str:
+    """Check if a word ends with punctuation and return the punctuation character."""
+    if word[-1] in PUNCTUATION:
+        return word[-1]
+    return ''
+
+def remove_punctuation(word: str) -> str:
+    """Remove punctuation from the end of a word."""
+    return word[:-1] if word[-1] in PUNCTUATION else word
+
+def is_uppercase(word: str) -> bool:
+    """Check if the word starts with an uppercase letter."""
+    return word[0].isupper()
+
+def translate_vowel_start(word: str) -> str:
+    """Translate words that start with a vowel by appending 'ay'."""
+    return word + 'ay'
+
+def translate_consonant_start(word: str) -> str:
+    """Translate words that start with a consonant by moving consonants to the end."""
+    consonant_prefix = ''
+    for i, char in enumerate(word):
+        if char.lower() not in VOWELS:
+            consonant_prefix += char
+        else:
+            word = word[i:]
+            break
+    return word + consonant_prefix.lower() + 'ay'
+
+def capitalize_word(word: str, is_uppercase: bool) -> str:
+    """Capitalize the word if it originally started with an uppercase letter."""
+    if is_uppercase:
+        return word.capitalize()
+    return word
+
 def consonant_translation(text: str) -> str:
-    vowels = "aeiouAEIOU"
+    """Translate a text into Pig Latin by handling vowels, consonants, and punctuation."""
     translated_words = []
 
     for word in text.split():
-        punctuation = ''
-        if word[-1] in string.punctuation:
-            punctuation = word[-1]
-            word = word[:-1]
+        # Handle punctuation
+        punctuation = has_punctuation(word)
+        word_without_punctuation = remove_punctuation(word)
 
-        is_uppercase = word[0].isupper()
+        # Check if the word starts with an uppercase letter
+        is_word_uppercase = is_uppercase(word_without_punctuation)
 
-        if word[0].lower() in vowels:
-            translated_word = word + 'ay'
+        # Translate based on whether the word starts with a vowel or consonant
+        if word_without_punctuation[0].lower() in VOWELS:
+            translated_word = translate_vowel_start(word_without_punctuation)
         else:
-            consonant_prefix = ''
-            for i, char in enumerate(word):
-                if char.lower() not in vowels:
-                    consonant_prefix += char
-                else:
-                    word = word[i:]
-                    break
+            translated_word = translate_consonant_start(word_without_punctuation)
 
-            translated_word = word + consonant_prefix.lower() + 'ay'
+        # Capitalize the word if needed and add punctuation back
+        translated_word = capitalize_word(translated_word, is_word_uppercase) + punctuation
 
-        if is_uppercase:
-            translated_word = translated_word.capitalize()
-
-        translated_word += punctuation
+        # Append the translated word to the list
         translated_words.append(translated_word)
 
     return ' '.join(translated_words)
